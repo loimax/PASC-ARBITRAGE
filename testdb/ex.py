483d3bdef596d80d7d7b522f23fa17bd786e0dae
Maxence <blazymaxence2018@gmail.com>
2022-06-17 22:41:23 +0200

    big update, added execute_query

--- testdb/ex.py
+++ testdb/ex.py
@@ -10,10 +10,19 @@ def create_connection(db_file):
     else:
         print("Error: Database not found")
         exit(1)
-      
+
+def execute_query(query, del_ins=False):
+    cursor.execute(query)
+    if del_ins:
+        conn.commit()
+    else:
+        conn.rollback()
+    return cursor  
+
 def show_tables():
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    result = cursor.fetchall()
+    query = "SELECT name FROM sqlite_master WHERE type='table';"
+    cur = execute_query(query)
+    result = cur.fetchall()
     i = 1
     for row in result:
         print(f"{i} : {row[0]}")
@@ -21,34 +30,66 @@ def show_tables():
 
 def display_attributes(name_table):
     cols = pd.read_sql_query(f"SELECT * from {name_table}", conn)
-    strtables = []
+    attr = []
     for cols in cols.columns:
-        strtables.append(cols)
-    print(strtables) #affiche liste complète attributs
+        attr.append(cols)
+    print(attr) #affiche liste complète attributs
 
-def display_table(name_table, specified=False, num_rows=0):
+def getAttributes(t_name):
+    cols = pd.read_sql_query(f"SELECT * from {t_name}", conn)
+    attr = []
+    for cols in cols.columns:
+        attr.append(cols)
+    return attr
     
-    cursor.execute(f"SELECT * FROM {name_table};")
-    cols = pd.read_sql_query(f"SELECT * from {name_table}", conn)
+def display_table(name_table, specified=False, num_rows=0):
+    query = f"SELECT * FROM {name_table};"
+    cur = execute_query(query)
+
+    attr = getAttributes(name_table)
+    attributes = " | ".join(attr)
+    print("\n", attributes, "\n")
+
     if specified:
-        result = cursor.fetchmany(num_rows)
+        result = cur.fetchmany(num_rows)
     else:
-        result = cursor.fetchall()
-        
-    strtables = " | ".join(cols.columns)
-    print("\n", strtables, "\n")
+        result = cur.fetchall()
+    
     for row in result:
-        print(row) #print(row[1]) affiche 1re colonne de la table
+        for i in range(len(row)):
+            print(row[i], end=" | ")
+        print("\n")
+        # print(row) #print(row[1]) affiche 1re colonne de la table
 
 def insert_value(name_table):
-    cursor.execute(f"INSERT INTO {name_table} VALUES (18, 'TestOpen', '2022/020203', '1200', '04180613', 3, 1)")
+    attr = getAttributes(name_table)
+
+    print("Number of values to insert: ", len(attr), "with values being: ", attr)
+    attrStr = ",".join(attr)
+    valToInsert = input("Entrez les valeurs à insérer séparées par des virgules : ")
+
+    query = f"INSERT INTO {name_table}({attrStr}) VALUES ({valToInsert})"
+    execute_query(query, True)
+
     conn.commit()
+
+def del_value(name_table): #bien utiliser un attribut de type int pour delete sinon marche pas
+    display_table(name_table)
+    name = input("Entrez le nom de l'attribut de l'élément: ")
+    value = input("Entrez la valeur de l'élément: ")
+    
+    query = f"DELETE FROM {name_table} WHERE {name} = {value}"
+    execute_query(query, False)
+
 
 
 conn = create_connection("testdb\GestionRegionale.db")
 cursor = conn.cursor()
 
-#show_tables()
-#display_attributes("CLUB")
-# insert_value("Epreuves")
-display_table("CLUB")
+
+# show_tables()
+# display_attributes("CLUB")
+
+# insert_value("sqlite_sequence")
+del_value("sqlite_sequence")
+display_table("sqlite_sequence")
