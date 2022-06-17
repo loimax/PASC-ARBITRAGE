a8f04800250fa8a232961cf03dfe9e3f416e2b33
Maxence Blazy <blazymaxence2018@gmail.com>
2022-06-17 11:35:39 +0200

    added tests

--- testdb/functions.py
+++ testdb/functions.py
@@ -8,9 +8,10 @@ def create_server_connection(host_name, user_name, user_password):
     try:
         connection = mysql.connector.connect(
             host=host_name,
-            user=user_name
+            user=user_name,
+            passwd=user_password
         )
-        print("MySQL Database connection successful")
+        print("MySQL Database connection successful to the server\n")
     except Error as err:
         print(f"Error: '{err}'")
 
@@ -20,19 +21,20 @@ def create_database(connection, query):
     cursor = connection.cursor()
     try:
         cursor.execute(query)
-        print("Database created successfully")
+        print("Database created successfully\n")
     except Error as err:
         print(f"Error: '{err}'")
 
-def create_db_connection(host_name, user_name, db_name):
+def create_db_connection(host_name, user_name, user_password, db_name):
     connection = None
     try:
         connection = mysql.connector.connect(
             host=host_name,
             user=user_name,
+            passwd=user_password,
             database=db_name
         )
-        print("MySQL Database connection successful")
+        print("MySQL Database connection successful to the database\n")
     except Error as err:
         print(f"Error: '{err}'")
 
@@ -43,7 +45,7 @@ def execute_query(connection, query):
     try:
         cursor.execute(query)
         connection.commit()
-        print("Query successful")
+        print(f"Query {query} successful\n")
     except Error as err:
         print(f"Error: '{err}'")
 
--- testdb/testdb.py
+++ testdb/testdb.py
@@ -3,11 +3,16 @@ from mysql.connector import Error
 import pandas as pd
 import functions as f
 
-connection = f.create_server_connection("localhost", "root", "Maxence")
-f.create_database(connection, "CREATE DATABASE IF NOT EXISTS testdb")
+pw = input("Enter your MySQL password: ")
+db = input("Enter your MySQL database name: ")
+
+
+connection = f.create_server_connection("localhost", "root", pw)
+create_db_query = f"CREATE DATABASE IF NOT EXISTS {db}"
+f.create_database(connection, create_db_query)
 
 create_teacher_table = """
-CREATE TABLE teacher (
+CREATE TABLE IF NOT EXISTS teacher (
   teacher_id INT PRIMARY KEY,
   first_name VARCHAR(40) NOT NULL,
   last_name VARCHAR(40) NOT NULL,
@@ -18,7 +23,7 @@ CREATE TABLE teacher (
   phone_no VARCHAR(20)
   );
  """
-
-connection = f.create_db_connection("localhost", "root", "testdb") # Connect to the Database
+connection = f.create_db_connection("localhost", "root", pw, db) # Connect to the Database
 f.execute_query(connection, create_teacher_table) # Execute our defined query
+
 
