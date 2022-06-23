474b0c0df285eec758f194e90c1030b80e8600c5
Lucas P <lucas.paillat@insa-cvl.fr>
2022-06-23 14:13:54 +0200

    rework de Affectation en OOP

--- Interface/Affectations.py
+++ Interface/Affectations.py
@@ -2,75 +2,70 @@ from tkinter import *
 import os
 from tkinter.ttk import Combobox
 from utils import *
-from setuptools import Command
 
 #tableau de 3 colonnes, les deux premieres normales (avec les équipes) et la 3e listes déroulantes pour sélectionner un JA
 
-def Affectation():
-    conn = create_connection("Interface/testdb/GestionRegionale.db")
-    cursor = conn.cursor()
-    #créer une fenetre
-    window = Tk()
-    #donner un titre a la window
-    window.title("Affectations")
-    #donner une taille a la Affectation
-    window.geometry("1920x1080")
-    
-    def quitter():
-        window.destroy()
+class Affectation():
+    def __init__(self):
+        conn = create_connection("Interface/testdb/GestionRegionale.db")
+        cursor = conn.cursor()
+        #créer une fenetre
+        self.main_window = Tk()
+        #donner un titre a la window
+        self.main_window.title("Affectations")
+        #donner une taille a la Affectation
+        self.main_window.geometry("1920x1080")
 
-    def generer():
+        #créer une liste de Affectations et les afficher 
+        self.nb_rencontres = 10
+        self.liste_Rencontres = ["Equipe 1", "Equipe 2", "Equipe 3", "Equipe 4", "Equipe 5", "Equipe 6", "Equipe 7", "Equipe 8", "Equipe 9", "Equipe 10"]
+
+        #liste_JA = ["Damien le Gamin", "Zemmour la pute", "Nezuko-chan", "Arthur le 5e", "LeDave"]
+        self.liste_JA = creation_liste(conn, cursor, "JA", ["PrenomJA","NomJA","NumLic"])
+
+        self.setup_texte()
+        self.setup_boutons()
+        self.creation_tableau()
+
+        self.main_window.attributes('-fullscreen', True)
+        self.main_window.mainloop()
+
+
+        
+    def creation_tableau(self):
+        for i in range(2): 
+            for j in range(self.nb_rencontres):
+                self.e = Entry(self.main_window, font=("Arial", 12))
+                self.e.place(x=642-184*1.5+i*184, y=105+j*20)
+                self.e.insert(END, self.liste_Rencontres[j])      
+                self.e.config(state="disabled")
+                self.f = Combobox(self.main_window, values=self.liste_JA, font=("Arial", 12))
+                self.f.place(x=642+184*0.5, y=105+j*20)
+                self.f.insert(END, "Juge Arbitre")
+                
+
+    def setup_texte(self):
+        txt = Label(self.main_window, text="Affectez un arbitre à chaque match :", font=("Arial", 15))
+        txt.place(x=642-162, y= 40)
+        
+        
+    def setup_boutons(self):
+        bouton_retour = Button(self.main_window, text="Retour", command=self.retour, bg='#AF7AC5', fg='#000000', font=('Arial', 10, 'bold'))
+        bouton_generer = Button(self.main_window, text="Générer", command=self.generer, bg='#AF7AC5', fg='#000000', font=('Arial', 12))
+        bouton_quitter = Button(self.main_window, text="Quitter", command=self.quitter, bg='#AF7AC5', fg='#000000', font=('Arial', 10, 'bold'))
+               
+        bouton_retour.place(x=25, y=680)
+        bouton_generer.place(x=642-38, y=680)
+        bouton_quitter.place(x=1200, y=680)
+
+
+    def quitter(self):
+        self.main_window.destroy()
+
+    def generer(self):
         print("Génération des convocations en cours")
-        window.destroy()
+        self.main_window.destroy()
 
-    #créer une liste de Affectations et les afficher 
-    nb_rencontres = 10
-    liste_Rencontres = ["Equipe 1", "Equipe 2", "Equipe 3", "Equipe 4", "Equipe 5", "Equipe 6", "Equipe 7", "Equipe 8", "Equipe 9", "Equipe 10"]
-
-    #liste_JA = ["Damien le Gamin", "Zemmour la pute", "Nezuko-chan", "Arthur le 5e", "LeDave"]
-    liste_JA = creation_liste(conn, cursor, "JA", ["PrenomJA","NomJA","NumLic"])
-
-    
-    class Table: #Pour faire un tableau
-        def __init__(self,Rencontre):
-            for i in range(2): 
-                for j in range(nb_rencontres):
-                    self.e = Entry(Rencontre, font=("Arial", 12))
-                    self.e.place(x=642-184*1.5+i*184, y=105+j*20)
-                    self.e.insert(END, liste_Rencontres[j])      
-                    self.e.config(state="disabled")
-
-
-            for j in range(nb_rencontres):
-                    self.f = Combobox(Rencontre, values=liste_JA, font=("Arial", 12))
-                    self.f.place(x=642+184*0.5, y=105+j*20)
-                    self.f.insert(END, "Juge Arbitre")
-    Table(window)
-    
-    def retour():
-        # bouton_retour.destroy()
-        window.destroy()
+    def retour(self):
+        self.main_window.destroy()
         os.system("python Interface/main.py")
-
-    #creer UI
-    txt = Label(window, text="Affectez un arbitre à chaque match :", font=("Arial", 15))
-    bouton_retour = Button(window, text="Retour", command=retour, bg='#AF7AC5', fg='#000000', font=('Arial', 10, 'bold'))
-    bouton_generer = Button(window, text="Générer", command=generer, bg='#AF7AC5', fg='#000000', font=('Arial', 12))
-    bouton_quitter = Button(window, text="Quitter", command=quitter, bg='#AF7AC5', fg='#000000', font=('Arial', 10, 'bold'))
-    
-
-    #afficher UI
-    txt.place(x=642-162, y= 40)
-    bouton_retour.place(x=25, y=680)
-    bouton_generer.place(x=642-38, y=680)
-    bouton_quitter.place(x=1200, y=680)
-
-    # window.update()
-    # print(bouton_generer.winfo_width())
-
-    #afficher la fenetre
-    window.attributes('-fullscreen', True)
-    window.mainloop()
-
-#afficher la fenetre
-Affectation()
--- Interface/main.py
+++ Interface/main.py
@@ -3,7 +3,7 @@ from Accueil import *
 def main():
     #Accueil()
     #Equipes()
-    Poules()
+    Affectation()
     #Matchs(["Bourges 2 ", "Nice 1 ", "Tour 1 ", "Manchon 4 ", "Tourcoin 1 ", "Paris 3 ", "Lannion 8 ", "Saint-Domingue 1 " ])
 
 if __name__ == "__main__":
