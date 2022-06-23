4d561db20dcaf6d714ae2d51abfed2c0d1302d9c
Guillaume Davy <Guillaume.Davy@insa-cvl.fr>
2022-06-23 14:08:52 +0200

    rencontres dans la database

--- Interface/main.py
+++ Interface/main.py
@@ -5,7 +5,7 @@ import sys
 def main():
     #Accueil()
     #Matchs(["Un", "Deux", "Trois", "Quatre", "Cinq", "Six", "Sept", "Huit" ])
-    Matchs(["Bourges 2 ", "Nice 1 ", "Tour 1 ", "Manchon 4 ", "Tourcoin 1 ", "Paris 3 ", "Lannion 8 ", "Saint-Domingue 1 " ])
+    Matchs(["VIERZON PING 2", "ASJ NOGENT LE ROTROU 1", "FRANCOURVILLE LD 1", "4S TOURS TT 1", "TT JOUE-LES-TOURS 1", "ES VILLEFRANCHE S/CHER 1", "CHATEAUNEUF TT 1", "FEROLLES VIENNE TT 1" ])
 
 if __name__ == "__main__":
     main()
