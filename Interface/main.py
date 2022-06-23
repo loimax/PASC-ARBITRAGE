256d2f819481b0efb5bdaab7674ff947ea06edc4
Lucas P <lucas.paillat@insa-cvl.fr>
2022-06-23 14:18:53 +0200

    pareil mais en mieux

--- Interface/main.py
+++ Interface/main.py
@@ -4,8 +4,6 @@ def main():
     #Accueil()
     #Equipes()
     #Affectation()
-    #Matchs(["Bourges 2 ", "Nice 1 ", "Tour 1 ", "Manchon 4 ", "Tourcoin 1 ", "Paris 3 ", "Lannion 8 ", "Saint-Domingue 1 " ])
-    #Matchs(["Un", "Deux", "Trois", "Quatre", "Cinq", "Six", "Sept", "Huit" ])
     Matchs(["VIERZON PING 2", "ASJ NOGENT LE ROTROU 1", "FRANCOURVILLE LD 1", "4S TOURS TT 1", "TT JOUE-LES-TOURS 1", "ES VILLEFRANCHE S/CHER 1", "CHATEAUNEUF TT 1", "FEROLLES VIENNE TT 1" ], "R3", "C", "2022", "1")
 
 if __name__ == "__main__":
