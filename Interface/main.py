216a12289dca7d28d6ab3527f0eaf93bc3f8921b
Rorhtax <91545444+Rorhtax@users.noreply.github.com>
2022-06-23 14:43:15 +0200

    Oubliez pas de remettre accueil dans main pitié

--- Interface/main.py
+++ Interface/main.py
@@ -1,9 +1,9 @@
 from Accueil import *
 
 def main():
-    #Accueil()
+    Accueil()
     #Matchs(["Un", "Deux", "Trois", "Quatre", "Cinq", "Six", "Sept", "Huit" ])
-    Matchs(["VIERZON PING 2", "ASJ NOGENT LE ROTROU 1", "FRANCOURVILLE LD 1", "4S TOURS TT 1", "TT JOUE-LES-TOURS 1", "ES VILLEFRANCHE S/CHER 1", "CHATEAUNEUF TT 1", "FEROLLES VIENNE TT 1" ])
+    #Matchs(["VIERZON PING 2", "ASJ NOGENT LE ROTROU 1", "FRANCOURVILLE LD 1", "4S TOURS TT 1", "TT JOUE-LES-TOURS 1", "ES VILLEFRANCHE S/CHER 1", "CHATEAUNEUF TT 1", "FEROLLES VIENNE TT 1" ])
 
 if __name__ == "__main__":
     main()
