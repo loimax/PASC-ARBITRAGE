465d2e11baeeeae306dad5922e7dd8f805039cc6
Arthur <arthur.mans@insa-cvl.fr>
2022-06-24 08:57:22 +0200

    style

--- Interface/utils.py
+++ Interface/utils.py
@@ -694,11 +694,11 @@ def getMaxValue(conn, cursor, name_table, attribute):
     return result[0][0]
 
 
-conn = create_connection("Interface/testdb/GestionRegionale.db")
-cursor = conn.cursor()
-# # update_tables(conn, cursor, ["JA"])
-# switchPhaseDuplicates(conn, cursor, "EquipeClub")
-display_table(conn,cursor,"EquipeClub")
+# conn = create_connection("Interface/testdb/GestionRegionale.db")
+# cursor = conn.cursor()
+# # # update_tables(conn, cursor, ["JA"])
+# # switchPhaseDuplicates(conn, cursor, "EquipeClub")
+# display_table(conn,cursor,"EquipeClub")
 # 162
 # # insert_entry(conn,cursor,"Rencontres",["1111","01845","78456","1","5","20/06/2022","17h00",""])
 # #
